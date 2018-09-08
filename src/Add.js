22a3ec3fcc4a7852838f75c734a715c499df50bd
ranckprogram <1474574022@qq.com>
2018-09-08 23:14:43 +0800

    父组件向子组件传值， 标签上自定义属性，子组件Props接收

--- src/Add.js
+++ src/Add.js
@@ -3,11 +3,12 @@ import React, {Component} from 'react';
 class Add extends Component {
 	constructor (props) {
 		super(props)
+		
+		console.dir(this)
 	}
 	
 	render () {
-		const numbers = [1, 2, 3, 4, 5];
-		const itemList = numbers.map((index, number) =>
+		const itemList = this.props.numbers.map((number, index) =>
 			<li key={index} onClick={this.handleClick}>{number}</li>
 		);
 		return (
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +7,9 @@ import Add from './Add.js'
 class App extends Component {
 	constructor (props) {
 		super(props)
+		this.state = {
+			numbers: [1, 5, 4, 2]
+		}
 		this.handleClick = this.handleClick.bind(this)
 	}
 	
@@ -14,22 +17,24 @@ class App extends Component {
 	handleClick () {
 		console.dir(1)
 	}
-  render() {
+	
+	render () {
 		
 		return (
-      <div className="App">
-				<Add/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-				
-      </div>
-    );
-  }
+			<div className="App">
+				<Add numbers={this.state.numbers} />
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+			
+			</div>
+		);
+	}
 }
+
 console.dir(App)
 export default App;
